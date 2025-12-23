55d9188e98f542172f3aa52917b5c7ac49a7f9f9
sonu-shivcharan <sonushivcharan@outlook.com>
2025-12-23 09:08:02 +0530

    update asynHandler to pass next

--- src/utils/async-handler.js
+++ src/utils/async-handler.js
@@ -1,6 +1,6 @@
 function asyncHandler(requestFn) {
   return (req, res, next) => {
-    Promise.resolve(requestFn(req, res)).catch((err) => next(err));
+    Promise.resolve(requestFn(req, res, next)).catch((err) => next(err));
   };
 }
 
