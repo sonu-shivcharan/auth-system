6b8b2b2d4730f899d74ff2e4fec289808c1f5499
sonu-shivcharan <sonushivcharan@outlook.com>
2025-12-26 20:03:07 +0530

    add custom logger using winston and morgan

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import logger from "./utils/logger.js";
+import morgan from "morgan";
 const app = express();
 
+const morganFormat = ":method :url :status :response-time ms";
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  }),
+);
 // basic configurations
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
--- src/controllers/healthCheck.controllers.js
+++ src/controllers/healthCheck.controllers.js
@@ -1,5 +1,6 @@
 import { ApiResponse } from "../utils/api-response.js";
 import { asyncHandler } from "../utils/async-handler.js";
+import logger from "../utils/logger.js";
 
 // const healthCheck = (req, res) => {
 //   try {
@@ -8,6 +9,7 @@ import { asyncHandler } from "../utils/async-handler.js";
 // };
 
 const healthCheck = asyncHandler(async (req, res) => {
+  logger.info("Health check");
   res.status(200).json(new ApiResponse(200, {}, "message"));
 });
 
